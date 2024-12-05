c7d1baa5e5bfa0791eec6dfca71d553b428e21e8
Dario <dario.guarracino2@gmail.com>
2024-12-05 23:21:36 +0000

    complete day 4 part 2

--- 4/main.cpp
+++ 4/main.cpp
@@ -11,29 +11,31 @@ bool isValid(vector<vector<char>>& matrix, int rows, int cols, int i, int j) {
 }
 
 
-bool dfs(vector<vector<char>>& matrix, int rows, int cols, int i, int j, int letterIndex, pair<int, int> direction) {
-
-	if (!isValid(matrix, rows, cols, i, j)) return false;
-	if (letters[letterIndex] == 'S' && matrix[i][j] == 'S') {
-		//cout << "XMAS!\n";
-		return true;
+void rotate(vector<vector<char>>& matrix,  int rows, int cols) {
+	//transpose
+	for (int i=0; i<rows; i++) {
+		for (int j=i+1; j<cols; j++) {
+			swap(matrix[i][j], matrix[j][i]);
+		}	
 	}
-	if (letterIndex >= letters.size()) return false;
 
-	int rowIdx = i + direction.first;
-	int colIdx = j + direction.second;
-	if (matrix[i][j] == letters[letterIndex]) {
-		//cout << format("Letter {}:\n", letters[letterIndex]);
-		//cout << format("({},{}): Going direction {},{}", i, j, direction.first, direction.second) << endl;	
-		return dfs(matrix, rows, cols, rowIdx, colIdx, letterIndex + 1, direction); 
-	} 
-
-	return false;
-
-	
-	
+	// reverse rows
+	for (int i=0; i<rows; i++) {
+		for(int j=0; j<cols/2; j++) {
+			swap(matrix[i][j], matrix[i][rows-j-1]);
+		}
+	}
 }
 
+void display(vector<vector<char>>& matrix) {
+	for (auto row: matrix) {
+		for (auto col: row) {
+			cout << col;
+		}
+		cout << endl;
+	}
+	cout << endl << endl;
+}
 
 int main(int argc, char** argv) {
 	ifstream file(argv[1]);
@@ -49,7 +51,7 @@ int main(int argc, char** argv) {
 		matrix.push_back(row);
 	} 
 
-	cout << format("Matrix size is {}x{}", matrix.size(), matrix[0].size());
+	cout << format("Matrix size is {}x{}\n", matrix.size(), matrix[0].size());
 	
 	// we need to find all the Xs, and from there, moving all possible directions to find the next letter ('M'. 
 	// then we move the SAME direction chosen until we find the rest of the letters ('A' and 'S')
@@ -57,21 +59,24 @@ int main(int argc, char** argv) {
 	const int cols = matrix[0].size();
 	int sum = 0;
 
-	for (int i=0; i<rows; i++) {
-		for (int j=0; j<cols; j++) {
-			char letterIndex = 0;
-			if (dfs(matrix, rows, cols, i, j, letterIndex, make_pair(0,1))) sum+= 1; // right
-			if (dfs(matrix, rows, cols, i, j, letterIndex, make_pair(1,1))) sum+=1; //bottom right
-			if (dfs(matrix, rows, cols, i, j, letterIndex, make_pair(1,0))) sum+=1; //bottom
-			if (dfs(matrix, rows, cols, i, j, letterIndex, make_pair(1,-1))) sum+=1; // bottom left
-			if (dfs(matrix, rows, cols, i, j, letterIndex, make_pair(0,-1))) sum+=1; //left
-			if (dfs(matrix, rows, cols, i, j, letterIndex, make_pair(-1,-1))) sum+=1; // top left
-			if (dfs(matrix, rows, cols, i, j, letterIndex, make_pair(-1,0))) sum+=1; //top
-			if (dfs(matrix, rows, cols, i, j, letterIndex, make_pair(-1,1))) sum+=1; //top right
-
+	for (int rot=0; rot<4; rot++) {
+		//display(matrix);
+		for (int i=0; i<rows-2; i++) {
+			for (int j=0; j<cols-2; j++) {
+				if (matrix[i][j] == 'M'
+					&& matrix[i][j+2] == 'S' 
+					&& matrix[i+2][j] == 'M'
+					&& matrix[i+2][j+2] == 'S'
+					&& matrix[i+1][j+1] == 'A') {
+						cout << format("X-MAS at ({},{})\n", i, j);
+						sum+=1;
+					}
 			
+			}
 		}
-	}
+		cout << "Rotating...\n";
+		rotate(matrix, rows, cols);
+	}	
 
 	cout << "Sum is " << sum << endl;
 
