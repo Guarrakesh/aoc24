c5336e5d9931e38e6efee10a460abc554587ecc4
Guarrakesh <darioguarracino@Darios-MacBook-Pro.local>
2024-12-09 23:56:54 +0000

    complete day 9...I'm tired boss

--- 9/main.cpp
+++ 9/main.cpp
@@ -5,17 +5,100 @@
 
 using namespace std;
 
-long long checksum(vector<string>& blocks) {
-    int id = 0;
+long long checksum(vector<string> &blocks)
+{
+    long long int id = 0;
     long long int sum = 0;
-    for (auto str: blocks) {
-        if (str == ".") continue;
+    for (auto str : blocks)
+    {
+        if (str == ".") {
+            id++;
+            continue;
+        }
         sum += id * stoi(str);
         id++;
-        
     }
     return sum;
+}
 
+void compact_blocks(vector<string> &blocks)
+{
+    int left = 0, right = blocks.size() - 1;
+
+    // Compactation
+    // Use two pointers, one at the beginning one at the end
+    // and iterate until they meet
+    while (left < right)
+    {
+
+        // free space stays at the end
+        if (blocks[right] == ".")
+        {
+            right--;
+            continue;
+        }
+        // we keep the occupied positions on the left until we find a free space
+        while (blocks[left] != ".")
+            left++;
+
+        // swap letter from the end with the space on the left
+        swap(blocks[left], blocks[right]);
+        right--;
+    }
+}
+
+void defragment(vector<string> &blocks)
+{
+    int left = 0, right = blocks.size() - 1;
+    while (right > 0)
+    {
+        if (blocks[right] == ".")
+        {
+            right--;
+            continue;
+        }
+
+        int rightProbe = right;
+        size_t fileSize = 0;
+        while (blocks[rightProbe] == blocks[right])
+        {
+            fileSize++;
+            rightProbe--;
+        }
+        // printf("FileSize = %zu (File = %s)\n", fileSize, blocks[right].c_str());
+        size_t freeSpace = 0;
+       
+      
+        
+        int leftProbe = 0; // we count how many free spaces ('.') we have from where left is
+        while (leftProbe < right && freeSpace < fileSize)
+        {
+            freeSpace = 0;
+            if (blocks[leftProbe] != ".") {
+                leftProbe++;
+                continue;
+            }
+            while (blocks[leftProbe++] == ".") {
+                freeSpace++;    
+            }
+          
+        }
+
+        // printf("FreeSpace=%zu\n", freeSpace);
+        int left = leftProbe-freeSpace-1;
+        // we have space, so move all file's file blocks to free space
+        if (freeSpace >= fileSize)
+        {
+            for (int i = 0; i < fileSize; i++)
+            {
+                swap(blocks[left++], blocks[right--]);
+            }
+        }
+        else
+        {
+            right -= fileSize;
+        }
+    }
 }
 int main(int argc, char **argv)
 {
@@ -23,7 +106,7 @@ int main(int argc, char **argv)
     string line;
 
     // we can do a simple array of chars (ie string) because we would use information
-    // about ID with multiple digits (>9). 
+    // about ID with multiple digits (>9).
     // With an vector of strings, each file block with its ID is going to be an element of the vector
     vector<string> blocks;
 
@@ -41,7 +124,8 @@ int main(int argc, char **argv)
         if (pos + 1 < line.size())
         {
             count = line[pos + 1] - '0'; // free space
-            while (count > 0) {
+            while (count > 0)
+            {
                 blocks.push_back(".");
                 count--;
             }
@@ -51,33 +135,21 @@ int main(int argc, char **argv)
         id++;
     }
 
-    // First step is done, now
-    for (auto str: blocks) cout << str; 
-    cout << endl;
+    // First step is done, now we compact.
+    // for (auto str: blocks) cout << str;
+    // cout << endl;
+    vector<string> blocks2;
+    copy(blocks.begin(), blocks.end(), back_inserter(blocks2));
 
-    int left = 0, right = blocks.size()-1;
+    compact_blocks(blocks);
+    defragment(blocks2);
 
-    // Defragment
-    // Use two pointers, one at the beginning one at the end 
-    // and iterate until they meet
-    while (left < right) {
-        // free space stays at the end
-        if (blocks[right] == ".") {
-            right--;
-            continue;
-        }
-        //we keep the occupied positions on the left until we find a free space
-        while (blocks[left] != ".") left++; 
-
-        // swap letter from the end with the space on the left
-        swap(blocks[left], blocks[right]);
-        right--;
-    }
-
-//    for (auto str: blocks) cout << str; 
-//    cout << endl;
-
+    // for (auto str : blocks2)
+    //     cout << str;
+    // cout << endl;
     auto res = checksum(blocks);
+    auto res2 = checksum(blocks2);
 
-    cout << "Res " << res << endl;
+    cout << "Res 1 " << res << endl;
+    cout << "Res 2 " << res2 << endl;
 }
